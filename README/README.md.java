ac6ea982ad63b4130089e70236cff17d5194999b
TegarVallen <tegarcibi66@gmail.com>
2025-10-27 09:40:27 +0700

    AlatHitungDasar

---
+++ README/README.md.java
@@ -0,0 +1,14 @@
+# 📱 KalkulatorApp
+
+Program **Kalkulator Sederhana** berbasis Java untuk melakukan operasi dasar aritmatika.
+
+## 🚀 Fitur
+- Penjumlahan
+- Pengurangan
+- Perkalian
+- Pembagian
+
+## 💻 Cara Menjalankan
+1. Clone repository ini:
+        ```bash
+git clone https://github.com/TegarVallen/KalkulatorApp.git
